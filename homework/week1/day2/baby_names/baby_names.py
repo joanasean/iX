dd144b849e64ae0f3cd22a54f063f40a57d2acaf
joanasean@gmail.com <joanasean@gmail.com>
2018-06-27 11:24:13 +0100

    finished

--- homework/week1/day2/baby_names/baby_names.py
+++ homework/week1/day2/baby_names/baby_names.py
@@ -10,5 +10,56 @@ This application will have the following functionalities:
 - It will accept a name as an argument
 - It will read a list of files (located in the folder data). Each file contains the
 most popular baby names for boys and girls for a certain year (the year is in the filename)
-- For the name provided as an argument, print out how many years it's been among the most popular among boys and girls
-"""
+# - For the name provided as an argument, print out how many years it's been among the most popular among boys and girls
+# """
+
+# path = '/Users/joanasean/Desktop/Data_Science/iX/homework/week1/day2/data/'
+# import glob
+# import os 
+
+# print (glob.glob ('Desktop/Data_Science/iX/homework/week1/day2/data*.txt'))
+
+# filenames = sorted(glob.glob ('data*.txt'))
+# filenames = filenames[:]
+
+# for f in filenames: 
+
+year = 1900
+mostnames = []
+# counting = []
+from collections import Counter 
+
+
+while year <= 2017: 
+	filename = "data/names_" + str(year)+ ".txt"
+	reading = (open (filename, 'r')).readline().split("|")
+	#print (reading)
+	mostnames.append(reading[1]) 
+	mostnames.append(reading[2].strip('\n'))
+	year += 1
+
+
+print (Counter(mostnames))
+
+
+
+	
+
+
+# for i in range(0,len(popularguys)):
+# 	set(zip(mostnamesguys[i],popularguys[i]))
+
+
+
+
+
+
+
+	
+
+	# mostnames.append(reading)
+
+
+
+
+
--- homework/week1/day2/tic_tac_toe/tic_tac_toe-exercise.py
+++ homework/week1/day2/tic_tac_toe/tic_tac_toe-exercise.py
@@ -27,10 +27,87 @@ are valid, i.e., they need to be empty and be inside the board.
 Hint: You can use a deque (in the module collections) to rotate between player 1 and 2
 """
 
+# board = [[1,2,3], 
+# 		[3,4,5],
+# 		[6,7,8]]
+
+# board = list(range(0,10))
+
+
+
+# winning_comb = [["0","0","0"],["1","1","1"]["2","2","2"],["0","1","2"],["2","1","0"]]
+# player_one = 'X'
+# player_two = 'Y'
+
+# print ('Welcome to Tic Tace Toe')
+
+# def choose_move():
+# 	print ('please choose a spot')
+# 	while True: 
+
+
+
+
+
+# # def player (move): if move 
+
+
+# def game():
+#     pass
+
+
+
+
+
+# if __name__ == "__main__":
+#     game()
+
+# x = list(range(0,10))
+# print (x)
+
+
+board = [None] + list(range(1,10))
+winning_comb = [(1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7)]
+
+def draw():
+		print(board[7], board[8], board[9])
+		print(board[4], board[5], board[6])
+	    print(board[1], board[2], board[3])
+	    print()
+
+def choose():
+	while True: 
+		try: 
+			a = int(input())
+			if a in board: 
+				return a 
+			else: 
+				print ("\nInvalid move. Try again")
+		except ValueError: 
+			print ("\nNot a number")
 
 def game():
-    pass
+	for a, b, c in winning_comb:
+		if board[a] == board[b] == board[c]:
+			print ("Player {0} wins!\n".format(board[a]))
+			return True
+	if sum((pos == 'X' or pos == 'O') for pos in board) == 9:
+		print("Tie\n")
+		return True 
+
+for player in 'XO' * 9:
+	draw()
+	if is_game_over():
+		break
+	print("Player {0} pick ")
+	board[choose_number()] = player
+
+while True: 
+	tic_tac_toe()
+	if input("play again (y/n)\n") != "y"
+		break 
 
 
-if __name__ == "__main__":
-    game()
+
+
+
